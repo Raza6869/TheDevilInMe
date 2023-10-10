05d00be516adf64b5c184185e31f1e197d1952b6
Rafael Thumaza <rassraza4@gmail.com>
2023-10-10 16:23:11 -0300

    feat: initial style configuration

---
+++ assets/fonts.js
@@ -0,0 +1,8 @@
+import { Gothic_A1, Andada_Pro } from "next/font/google";
+
+export const gothic = Gothic_A1({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const andadaPro = Andada_Pro({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        darkgray: "#0B0E0F",
+        lightgray: "#D9D9D9",
+        redbase: "#C90000",
+        darkred: "#7B0909",
+        footer: "#414141",
       },
     },
   },
   plugins: [],
-}
+};
