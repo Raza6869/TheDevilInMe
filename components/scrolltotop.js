3edc1f8b7ea1b5f9c174bd7012ff5c6d303e2311
Rafael Thumaza <rassraza4@gmail.com>
2023-10-27 09:19:42 -0300

    organization

--- components/scrolltotop.js
+++ components/scrolltotop.js
@@ -1,12 +1,6 @@
+"use client";
 //next imports
 import { motion } from "framer-motion";
-
-const scroll = window.screen.height;
-
-console.log(scroll);
-
-const inView = "visible";
-const hidden = "invisible";
 
 export default function ScrollToTop() {
   return (
@@ -15,8 +9,7 @@ export default function ScrollToTop() {
       initial={{ opacity: 0, scale: "80%" }}
       animate={{ opacity: 1, scale: "100%" }}
       transition={{ type: "spring" }}
-      className={`
-       bg-redbase px-4 py-3 text-xl cursor-pointer w-fit text-bold text-white fixed rounded-full right-10 bottom-10`}
+      className="bg-redbase px-4 py-3 text-xl cursor-pointer w-fit text-bold text-white fixed rounded-full right-10 bottom-10 z-50"
     >
       <i class="bi bi-arrow-up"></i>
     </motion.a>
